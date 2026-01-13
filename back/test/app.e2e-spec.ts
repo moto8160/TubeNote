d5e1f320de64f3c5d745d1d11cf81b0eefa316a4
mori <hokuto.8160@gmail.com>
2026-01-13 22:34:31 +0900

    add test

--- back/test/app.e2e-spec.ts
+++ back/test/app.e2e-spec.ts
@@ -4,19 +4,51 @@ import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('posts E2E', () => {
   let app: INestApplication<App>;
+  let accessToken: string;
 
-  beforeEach(async () => {
+  // テスト開始前に1回実行
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    // テスト用ユーザーでログイン
+    const loginRes = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: 'testuser@example.com',
+        password: 'testuser',
+        remember: false,
+      })
+      .expect(201);
+    accessToken = loginRes.body.access_token;
   });
 
-  it('GET /posts は未認証だと 401', async () => {
-    await request(app.getHttpServer()).get('/posts').expect(401);
+  it('未認証の時、401', async () => {
+    await request(app.getHttpServer()).get('/posts').expect(401); //Unauthorized
+  });
+
+  it('post作成', async () => {
+    await request(app.getHttpServer())
+      .post('/posts')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        videoUrl: 'https://www.youtube.com/watch?v=esa7AFGWNeU',
+        text: 'test',
+      })
+      .expect(201); //Created
+  });
+
+  it('一覧取得', async () => {
+    const res = await request(app.getHttpServer())
+      .get('/posts')
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    console.log(res.body);
   });
 });
--- back/test/test-auth.guard.ts
+++ back/test/test-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TestAuthGuard extends AuthGuard('jwt') {}
+export class TestJwtAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+
+    // 認証済みユーザーを注入
+    request.user = {
+      userId: 1,
+      email: 'test@example.com',
+    };
+
+    return true; // 常に通す
+  }
+}
