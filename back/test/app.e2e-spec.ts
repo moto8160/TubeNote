7c382395fb4d09bf48f3e74d780cfa40e0bdb46d
mori <hokuto.8160@gmail.com>
2026-01-14 21:43:48 +0900

    add test

--- back/test/app.e2e-spec.ts
+++ back/test/app.e2e-spec.ts
@@ -7,6 +7,7 @@ import { AppModule } from './../src/app.module';
 describe('posts E2E', () => {
   let app: INestApplication<App>;
   let accessToken: string;
+  let postId: number;
 
   // テスト開始前に1回実行
   beforeAll(async () => {
@@ -18,7 +19,7 @@ describe('posts E2E', () => {
     await app.init();
 
     // テスト用ユーザーでログイン
-    const loginRes = await request(app.getHttpServer())
+    const res = await request(app.getHttpServer())
       .post('/auth/login')
       .send({
         email: 'testuser@example.com',
@@ -26,7 +27,9 @@ describe('posts E2E', () => {
         remember: false,
       })
       .expect(201);
-    accessToken = loginRes.body.access_token;
+
+    const body = res.body as { access_token: string };
+    accessToken = body.access_token;
   });
 
   it('未認証の時、401', async () => {
@@ -34,21 +37,54 @@ describe('posts E2E', () => {
   });
 
   it('post作成', async () => {
-    await request(app.getHttpServer())
+    const res = await request(app.getHttpServer())
       .post('/posts')
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
-        videoUrl: 'https://www.youtube.com/watch?v=esa7AFGWNeU',
+        videoUrl: 'https://www.youtube.com/watch?v=TRcquLFgiWU',
         text: 'test',
       })
       .expect(201); //Created
+
+    const body = res.body as { postId: number };
+    postId = body.postId;
   });
 
   it('一覧取得', async () => {
-    const res = await request(app.getHttpServer())
+    await request(app.getHttpServer())
       .get('/posts')
-      .set('Authorization', `Bearer ${accessToken}`);
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200); //OK
+  });
 
-    console.log(res.body);
+  it('詳細取得', async () => {
+    const res = await request(app.getHttpServer()).get(`/posts/${postId}`).expect(200);
+
+    const body = res.body as { id: number };
+    expect(body.id).toBe(postId);
+  });
+
+  it('post更新', async () => {
+    const res = await request(app.getHttpServer())
+      .put(`/posts/${postId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        text: 'test updated',
+      })
+      .expect(200);
+
+    const body = res.body as { success: boolean };
+    expect(body.success).toBe(true);
+  });
+
+  it('post削除', async () => {
+    const res = await request(app.getHttpServer())
+      .delete(`/posts/${postId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200);
+
+    const body = res.body as { success: boolean; videoDeleted: boolean };
+    expect(body.success).toBe(true);
+    expect(body.videoDeleted).toBe(true);
   });
 });
